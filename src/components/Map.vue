2c0694a444bf82f12e5b428e68225494a5c8debb
Adro <acousens@gmail.com>
2020-04-13 16:11:55 -0700

    Assign photos based on use

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -107,18 +107,33 @@
           this.markers[item.place.id] = marker;
         });
       },
-      randomPhoto() {
+      nextAvailablePhoto(googleMapPlace) {
         let photos = this.$store.state.photos, keys = Object.keys(photos);
-        let randomNumber = Math.floor(Math.random() * (keys.length - 1));
-        return photos[keys[randomNumber]];
+        var i = 0, nextAvailable = null;
+        while (nextAvailable === null) {
+          var thisPhoto = photos[keys[i]];
+          if (thisPhoto.googleMapPlaces.length === 0 ) {
+            nextAvailable = thisPhoto
+          } else if (i === keys.length-1) {
+            nextAvailable = photos[keys[0]]
+          } else {
+            var nextPhoto = photos[keys[i+1]]
+            if (nextPhoto.googleMapPlaces.length < thisPhoto.googleMapPlaces.length) {
+              nextAvailable = nextPhoto
+            } else {
+              i++
+            }
+          }
+        }
+        this.$store.dispatch('addGoogleMapPlaceIDToPhoto', {photoID: nextAvailable.id, googleMapPlaceID: googleMapPlace.id})
+        return nextAvailable;
       },
       getOrCreateLocation(googleMapPlace) {
         let locations = this.$store.state.map.locations;
         let location = locations[googleMapPlace.id];
         if (typeof location === 'undefined') {
           let price = `$${(Math.floor(Math.random() * 10000000)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`;
-          // TODO: get next available photo instead of random photo
-          let photo = this.randomPhoto()
+          let photo = this.nextAvailablePhoto(googleMapPlace)
           location = {
             place: googleMapPlace,
             photo: photo,
--- src/store/index.js
+++ src/store/index.js
@@ -11,11 +11,17 @@ export default new Vuex.Store({
   mutations: {
     addPhotos (state, payload) {
       state.photos = payload.photos
+    },
+    addGoogleMapPlaceIDToPhoto (state, payload) {
+      state.photos[payload.photoID].googleMapPlaces.push(payload.googleMapPlaceID)
     }
   },
   actions: {
     addPhotos (context, payload) {
       context.commit('addPhotos', payload)
+    },
+    addGoogleMapPlaceIDToPhoto (context, payload) {
+      context.commit('addGoogleMapPlaceIDToPhoto', payload)
     }
   },
   modules: {
