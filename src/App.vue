24429925ac3a0ff6961cb46c1425c75175b9abc0
Adro <acousens@gmail.com>
2020-04-15 21:22:56 -0700

    Fetch all photos from collection

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,49 @@
   const axios = require('axios').default
   export default {
     name: 'App',
+    data () {
+      return {
+        secrets: null,
+        page: 1,
+        photos: {}
+      }
+    },
+    methods: {
+      getPhotos(resolve, reject) {
+        var collection = 'https://api.unsplash.com/collections/9881644/photos';
+        var url = `${collection}?page=${this.page}&per_page=30&orientation=landscape&query=castles&client_id=${this.secrets.data.access}&client_secret=${this.secrets.data.secret}`;
+
+        axios.get(url).then((resp) => {
+          resp.data.forEach((item) => {
+            item.googleMapPlaces = []
+            this.photos[item.id] = item
+          })
+          if (resp.data.length < 30 ) {
+            this.secrets = null;
+            resolve()
+          } else {
+            this.page++
+            this.getPhotos(resolve, reject)
+          }
+        })
+      }
+    },
     created: function() {
-      // Local? Run: `npm run unsplash`
       let unsplashSecretsURL = 'https://us-central1-thisadrian.cloudfunctions.net/unsplash-secrets';
       if (window.location.hostname === 'localhost') {
         unsplashSecretsURL = 'http://localhost:8000/'
       }
+
       axios.get(unsplashSecretsURL).then((resp) => {
-        let perPage = 30, query = 'castles', api = 'https://api.unsplash.com/collections/9881644/photos';
-        let url = `${api}?page=1&per_page=${perPage}&orientation=landscape&query=${query}&client_id=${resp.data.access}&client_secret=${resp.data.secret}`
-        axios.get(url).then((resp) => {
-          let photos = {};
-          resp.data.forEach((item) => {
-            item.googleMapPlaces = []
-            photos[item.id] = item
-          })
-          this.$store.dispatch('addPhotos', {photos: photos})
+        this.secrets = resp;
+        new Promise((resolve, reject) => {
+          this.getPhotos(resolve, reject)
+        }).then(() => {
+          this.$store.dispatch('addPhotos', {photos: this.photos})
         })
       }).catch((err) => {
         console.log(err)
       })
-
-
     }
   }
 </script>
