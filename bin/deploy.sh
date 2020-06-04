c69872b4e4a12c60b8d6b53ef8b39065d693b433
Adro <acousens@gmail.com>
2020-06-04 13:54:01 -0700

    Add must-revalidate to cache control

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,7 @@ gsutil -m rsync -d -x "^\.|.*md$|.*js$|.*json$" ./dist gs://zcastles.thisadrian.
 ;gsutil -m rsync -d -r -x "^\." ./dist/static gs://zcastles.thisadrian.com/static
 
 # Set cache control
-;gsutil -m setmeta -r -h "Cache-Control:no-cache,max-age=0" gs://zcastles.thisadrian.com
-;gsutil -m setmeta -r -h "Cache-Control:no-cache,max-age=0" gs://zcastles.thisadrian.com/css
-;gsutil -m setmeta -r -h "Cache-Control:no-cache,max-age=0" gs://zcastles.thisadrian.com/js
-;gsutil -m setmeta -r -h "Cache-Control:no-cache,max-age=0" gs://zcastles.thisadrian.com/static
+;gsutil -m setmeta -r -h "Cache-Control:no-cache, max-age=0, must-revalidate" gs://zcastles.thisadrian.com
+;gsutil -m setmeta -r -h "Cache-Control:no-cache, max-age=0, must-revalidate" gs://zcastles.thisadrian.com/css
+;gsutil -m setmeta -r -h "Cache-Control:no-cache, max-age=0, must-revalidate" gs://zcastles.thisadrian.com/js
+;gsutil -m setmeta -r -h "Cache-Control:no-cache, max-age=0, must-revalidate" gs://zcastles.thisadrian.com/static
