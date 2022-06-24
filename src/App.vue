519f553c6878097c1a145aecca18bdfa7733a797
rttll <adrianxyz@proton.me>
2022-06-23 20:40:32 -0700

    vite init

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,21 @@
-<template>
-  <main id="app">
-    <router-view/>
-  </main>
-</template>
-
-<script type="text/javascript">
-  import remote from '@/services/remote-api-proxy.js'
-  export default {
-    name: 'App',
-    data () {
-      return {
-        secrets: null,
-        page: 1,
-        photos: {}
-      }
-    },   
-    created: function() { 
-      // TODO: Could refresh after x days 
-      const photos = localStorage.getItem('zcastles-photos')
-      if (photos !== null) {
-        this.$store.dispatch('addPhotos', {photos: JSON.parse(photos)})
-      } else {
-        remote.photos().then((resp) => {
-          this.$store.dispatch('addPhotos', {photos: resp})
-        }).catch((err) => {
-          console.log(err)
-        })
-      }
-    }
-  }
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
-  @import '@/assets/scss/app.scss'
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-import VueCompositionApi from '@vue/composition-api';
-
-Vue.use(VueCompositionApi);
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
